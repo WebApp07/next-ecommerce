5f4f14e2ece89180ecdf7c3ca309da06f5b169ae
unknown <aminezouguig@proton.me>
2024-09-02 18:59:11 +0200

    Filter Product Options, Product Quantity and Add to cart Button

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -3,11 +3,10 @@ import CustomizeProducts from "@/components/CustomizeProducts";
 import ProductImages from "@/components/ProductImages";
 import { wixClientServer } from "@/lib/wixClientServer";
 import { notFound } from "next/navigation";
-import React from "react";
 
 const SinglePage = async ({ params }: { params: { slug: string } }) => {
-  console.log(params.slug);
   const wixClient = await wixClientServer();
+
   const products = await wixClient.products
     .queryProducts()
     .eq("slug", params.slug)
@@ -20,12 +19,11 @@ const SinglePage = async ({ params }: { params: { slug: string } }) => {
   const product = products.items[0];
 
   return (
-    <div className="px-4 md:px-8 lg:px-16  xl:px-32 2xl:px-32 relative flex flex-col lg:flex-row gap-16">
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
       {/* IMG */}
       <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
         <ProductImages items={product.media?.items} />
       </div>
-
       {/* TEXTS */}
       <div className="w-full lg:w-1/2 flex flex-col gap-6">
         <h1 className="text-4xl font-medium">{product.name}</h1>
@@ -44,16 +42,29 @@ const SinglePage = async ({ params }: { params: { slug: string } }) => {
           </div>
         )}
         <div className="h-[2px] bg-gray-100" />
-        <CustomizeProducts />
-        <Add />
-
+        {product.variants && product.productOptions ? (
+          <CustomizeProducts
+            productId={product._id!}
+            variants={product.variants}
+            productOptions={product.productOptions}
+          />
+        ) : (
+          <Add
+            productId={product._id!}
+            variantId="00000000-0000-0000-0000-000000000000"
+            stockNumber={product.stock?.quantity || 0}
+          />
+        )}
         <div className="h-[2px] bg-gray-100" />
         {product.additionalInfoSections?.map((section: any) => (
           <div className="text-sm" key={section.title}>
-            <h4 className="font-medium mb-4">Title</h4>
+            <h4 className="font-medium mb-4">{section.title}</h4>
             <p>{section.description}</p>
           </div>
         ))}
+        <div className="h-[2px] bg-gray-100" />
+        {/* REVIEWS */}
+        <h1 className="text-2xl">User Reviews</h1>
       </div>
     </div>
   );
