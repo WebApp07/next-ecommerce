fc29a2e2965737cf630afd93a0f7c177d50cf3bb
unknown <aminezouguig@proton.me>
2024-09-02 21:42:08 +0200

    feat: Integrate authentication with Wix Headless

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link";
-import Image from "next/image";
-import React from "react";
 import Menu from "./Menu";
+import Image from "next/image";
 import SearchBar from "./SearchBar";
-import NavIcons from "./NavIcons";
+import dynamic from "next/dynamic";
+// import NavIcons from "./NavIcons";
+
+const NavIcons = dynamic(() => import("./NavIcons"), { ssr: false });
 
 const Navbar = () => {
   return (
-    <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-32 relative">
+    <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+      {/* MOBILE */}
       <div className="h-full flex items-center justify-between md:hidden">
-        {/*MOBILE*/}
         <Link href="/">
           <div className="text-2xl tracking-wide">LAMA</div>
         </Link>
@@ -22,14 +24,14 @@ const Navbar = () => {
           <Link href="/" className="flex items-center gap-3">
             <Image src="/logo.png" alt="" width={24} height={24} />
             <div className="text-2xl tracking-wide">LAMA</div>
-            <div className="hidden xl:flex gap-4">
-              <Link href="/">Homepage</Link>
-              <Link href="/">Shop</Link>
-              <Link href="/">Deals</Link>
-              <Link href="/">About</Link>
-              <Link href="/">Contact</Link>
-            </div>
           </Link>
+          <div className="hidden xl:flex gap-4">
+            <Link href="/">Homepage</Link>
+            <Link href="/">Shop</Link>
+            <Link href="/">Deals</Link>
+            <Link href="/">About</Link>
+            <Link href="/">Contact</Link>
+          </div>
         </div>
         {/* RIGHT */}
         <div className="w-2/3 xl:w-1/2 flex items-center justify-between gap-8">
